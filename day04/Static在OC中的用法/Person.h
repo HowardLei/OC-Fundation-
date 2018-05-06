1f2bc73be782e6b29a7e6aad060dcc6cd407601b
HowardLei <jyz@myimac7.local>
2018-05-06 10:48:26 +0800

    Static的用法

---
+++ day04/Static在OC中的用法/Person.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+{
+//    static int _age; 这种写法是错误的。没有这么写的
+}
+
+@end
---
+++ day04/Static在OC中的用法/main.m
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+/*
+ 在 OC 中，static 的用法与 C 相同。
+ 但是，有些地方不能用
+ 1、不能修饰成员变量.
+ 2、不能修饰方法。
+ 3、不能修饰OC对象.
+ */
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
