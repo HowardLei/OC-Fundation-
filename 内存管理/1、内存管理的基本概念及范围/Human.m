72ea58f192cd12b4fa56de9c00b6a8734345454b
HowardLei <jyz@myimac7.local>
2018-05-11 19:45:39 +0800

    有关内存管理的基本代码

--- 内存管理/1、内存管理的基本概念及范围/Human.m
+++ 内存管理/1、内存管理的基本概念及范围/Human.m
@@ -1,5 +1,8 @@
 #import "Human.h"
 
 @implementation Human
-
+- (void)dealloc{
+    NSLog(@"人对象没所有者了……被回收了");
+    [super dealloc];
+}
 @end
--- 内存管理/1、内存管理的基本概念及范围/main.m
+++ 内存管理/1、内存管理的基本概念及范围/main.m
@@ -39,30 +39,39 @@ void ReferenceCounting(){
      MRC: Mannul reference counting 手动引用计数法
      在 MRC 模式下，一共有 3 种方法：
      1、retain 方法。该方法的作用是将内存数据的所有权附给另一指针变量，引用数加1，即retainCount+= 1;（直接写的话相当于给了一个虚拟空间？？？）
+     2、release 方法。该方法是释放指针变量对内存数据的所有权，引用数减1，即retainCount -= 1;注意：release并不代表销毁\回收对象，仅仅是计数器-1。当这个对象的引用计数为 0 的时候，系统会调用对象的 dealloc 方法释放他的内存。
+     3、autorelease，该方法是将该对象内存的管理放到autoreleasepool中。（只是先放到池子里，暂时不销毁对象）
      */
 }
 // MARK: 内存管理基础代码
 void demo1(){
-    int a = 10;
-    int b = 20;
-    Human *h = [[Human alloc] init]; // 程序结束后，这行代码中指针变量将会被回收，但是对象的内存系统并不会被主动回收，需要我们自己进行回收。
+//    int a = 10;
+//    int b = 20;
+//    Human *h = [[Human alloc] init]; // 程序结束后，这行代码中指针变量将会被回收，但是对象的内存系统并不会被主动回收，需要我们自己进行回收。
 }
 // MARK: 内存管理分配代码
 void demo2(){
-    // 创建一个人的对象，默认引用计数器就为 1 。
-    Human *h = [[Human alloc] init];
-    NSLog(@"%lu", [h retainCount]);
-    // 向 h 发送 retain 消息，引用计数器就 +1 。
-    Human *p = [h retain];
-    // 所以现在 [h retainCount]为 2 。
-    NSLog(@"%lu", [h retainCount]);
-    // 向 h 发送 release 消息，引用计数器就 -1 。
+    // 创建一个人对象，用 p 指针变量接收，其默认引用计数为 1
+    Human *p = [[Human alloc] init];
+    // 检查 p 的引用计数
+    NSLog(@"现在引用计数为%lu", [p retainCount]);
+    // 将内存数据的所有权附给另一指针变量 s，引用数 +1
+    Human *s = [p retain];
+    // 现在这个人对象应该是有 2 个指针拥有它的所有权
+    NSLog(@"现在引用计数为%lu", [p retainCount]);
+    // 现在让 s 指针释放他对内存数据的所有权，引用数 -1
+    [s release];
+    // 现在这个人对象应该是有 1 个人拥有它的所有权。即：p
+    NSLog(@"现在引用计数为%lu", [p retainCount]);
+    // 让 p 指针 释放他对内存的使用权，引用数 -1
     [p release];
-    // 所以现在 [h retainCount]为 1 。
-    NSLog(@"%lu", [h retainCount]);
-    // 再向 h 发送 release 消息，引用计数器再 -1 。注意这个时候只是计数器为 0 。并不代表销毁/回收对象。需要重写 dealloc 方法才能进行
-    [h release];
-    NSLog(@"%lu", [h retainCount]);
+    // 现在人对象的引用计数为 0 ，现在系统会调用对象的 dealloc 方法。释放内存。
+    //p 指针、s 指针 指向的对象已经被回收掉了，于是成了野指针。
+    // 这个被释放的对象称为僵尸对象。
+    // 当1个指针变为野指针以后. 就把这个指针的值设置为nil 防止他乱指。
+    p = s = nil;
+    // 当他是空指针的时候，调用方法都没事。
+    NSLog(@"现在引用计数为%lu", [p retainCount]);
 }
 void try(){
     Dog *d = [Dog new];
