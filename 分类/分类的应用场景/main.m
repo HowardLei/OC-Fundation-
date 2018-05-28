f2c7970c5850688d5ee46034661703dc32e15116
HowardLei <jyz@myimac7.local>
2018-05-28 20:50:22 +0800

    非正式协议

--- 分类/分类的应用场景/main.m
+++ 分类/分类的应用场景/main.m
@@ -3,7 +3,7 @@
 #import "NSObject+sport.h"
 #import "NSString+countNum.h"
 /*
- 分类的应用场景：扩展系统类的方法。（因为假如你创建的大多数的对象都需要此方法，但是系统内没有内置此方法，你就可以创建分类）
+ 分类的应用场景：扩展系统类的方法。（因为假如你创建的大多数的对象都需要此方法，但是系统内没有内置此方法，你就可以创建此分类的非正式协议）
  */
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
