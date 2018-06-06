fbaa087935fda10d321a38eec22e8d6b087b93f8
HowardLei <jyz@myimac7.local>
2018-06-06 21:02:30 +0800

    NSString类的构造方法以及比较方法

--- Foundation库/NSString类方法/main.m
+++ Foundation库/NSString类方法/main.m
@@ -1,6 +1,12 @@
 #import <Foundation/Foundation.h>
 // 关于字符串，先去学习基础的代码，等到后边在应用的时候，需要啥再去学习新的。然后总结在一个文件中
 // 注意：NSString 类是不可变字符串
+NSString *getNSString() {
+    char str[256];
+    NSLog(@"请输入一个字符串");
+    fgets(str, 255 * sizeof(char), stdin);
+    return [NSString stringWithUTF8String:str];
+}
 // MARK: 字符串的添加
 void add() {
     // 1、从文本文件中获取字符串
@@ -15,19 +21,33 @@ void add() {
         NSLog(@"%@", err);
     }
     // 2、通过 URL 获取字符串（URL:统一资源定位符,用于定位网上的资源。构成：协议://主机:端口/路径?参数）
-
+    NSURL *p = [[NSURL alloc] initWithString:@"https://www.baidu.com/"];
+    NSError *err1;
+    NSString *str1 = [[NSString alloc] initWithContentsOfURL:p encoding:NSUTF8StringEncoding error:&err1];
+    if (!err1) {
+        NSLog(@"%@", str1);
+    } else {
+        NSLog(@"%@", err1);
+    }
+    // 3、格式化字符串。用于将其他类型的数据转化为字符串。用法相当与 NSLog，有类方法和对象方法。
+    NSString *str2 = [NSString stringWithFormat:@"%d + %d = %d", 1 , 2, 1 + 2];
+    NSLog(@"%@", str2);
+    // 4、从 C 语言获得字符串，并将其转化为 OC 字符串
+    NSString *str3 = getNSString();
+    NSLog(@"%@", str3);
 }
-// MARK: 字符串的删除
-void delete() {
-
-}
-// MARK: 字符串的修改
-void alter() {
-
-}
+// MARK: 字符串的删除（NSString 中没有）
+// MARK: 字符串的修改（NSString 中没法修改）
 // MARK: 字符串的查找
 void search() {
-
+    //OC 中比较两个字符串的方法：1、isEqualToString 2、compare
+    NSString *str1 = @"132";
+    NSString *str2 = @"123";
+    NSString *str3 = @"132";
+    BOOL a = [str1 isEqualToString:str2];
+    BOOL b = [str1 isEqualToString:str3];
+    BOOL c = [str2 isEqualToString:str3];
+    NSLog(@"%d %d %d", a, b, c);
 }
 // MARK: 字符串的其他方法
 void others() {
@@ -36,9 +56,9 @@ void others() {
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // 1、NSString 类的构造方法
-        add();
-        delete();
+//        add();
         search();
+        others();
     }
     return 0;
 }
