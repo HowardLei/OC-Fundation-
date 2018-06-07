fed8ec3a4631c96ca0d9b9491f95bbc1a266520a
HowardLei <jyz@myimac7.local>
2018-06-07 17:41:05 +0800

    NSString 类搜索方法

--- Foundation库/NSString类方法/main.m
+++ Foundation库/NSString类方法/main.m
@@ -7,6 +7,13 @@ NSString *getNSString() {
     fgets(str, 255 * sizeof(char), stdin);
     return [NSString stringWithUTF8String:str];
 }
+NSString *(^myBlock)(BOOL) = ^(BOOL a) {
+    if (a) {
+        return @"真";
+    } else {
+        return @"假";
+    }
+};
 // MARK: 字符串的添加
 void add() {
     // 1、从文本文件中获取字符串
@@ -40,12 +47,30 @@ void add() {
 // MARK: 字符串的修改（NSString 中没法修改）
 // MARK: 字符串的查找
 void search() {
-    //OC 中比较两个字符串的方法：1、isEqualToString 2、compare
-    NSString *str1 = @"132";
-    NSString *str2 = @"123";
-    NSString *str3 = @"132";
-    NSComparisonResult a = [str1 compare:str3];
-    NSLog(@"%ld", a);
+    NSString *str1 = @"123";
+    NSString *str2 = @"456";
+    NSString *str3 = @"abc";
+    NSString *str4 = @"ABC";
+    NSString *str5 = @"https://www.baidu.com/";
+    NSString *str6 = @"/Users/jyz/Desktop";
+    //A、OC 中比较两个字符串的方法：1、isEqualToString 方法 2、compare 方法
+    // 1、用 isEqualToString 方法 返回值为 BOOL 类型 （注意：BOOL 类型中 1 为真，0 为假）
+    BOOL a = [str1 isEqualToString:str2];
+    NSLog(@"%@", myBlock(a));
+    // 2、用 compare 方法比较，他的比较方法是字典比较法，就是将字符串的每个元素的 ASCII 码进行比较，如果出现不同，则停止比较。返回值为 NSComparisonResult 类型（它本身是枚举）注意：如果后边有附加条件的话，直接加 options: 后边有参数。
+    NSComparisonResult b = [str1 compare:str2];
+    NSComparisonResult c = [str3 compare:str4 options:1];
+    NSLog(@"%ld", c);
+    NSLog(@"%ld", b);
+    //B、查找字符串中是不是有前缀后缀。查前缀  hasPrefix 方法，查后缀 hasSuffix 方法。返回值同样是 BOOL
+    BOOL d = [str5 hasPrefix:@"https://"];
+    BOOL e = [str6 hasSuffix:@"Desktop"];
+    NSLog(@"%@,%@", myBlock(d), myBlock(e));
+    //C、在字符串中搜索字符串 rangeOfString ，返回值为：NSRange 类型（NSRange 本事是结构体，有 location 值：即子字符串在主字符串中第1次出现的下标，length 值：即匹配到了多少个长度的字符）。注意：默认从前往后搜，如果从后往前搜，那就在后面的参数上加option :2
+    NSRange f = [str5 rangeOfString:@"baidu"];
+    NSLog(@"baidu在str5这个字符串的位置为：%lu，长度为：%lu", f.location, f.length);
+    NSRange g = [str6 rangeOfString:@"Desktop" options:2];
+    NSLog(@"%lu", g.length);
 }
 // MARK: 字符串的其他方法
 void others() {
