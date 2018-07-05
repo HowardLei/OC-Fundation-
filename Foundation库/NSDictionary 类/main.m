c7e41997178ea4226c668d1ce9ae00e3fcb231fd
HowardLei <410905481@qq.com>
2018-07-05 20:44:40 +0800

    NSDictionary 的基础知识

--- Foundation库/NSDictionary 类/main.m
+++ Foundation库/NSDictionary 类/main.m
@@ -10,10 +10,22 @@
     这种存储数据的方式，叫做“键值对”。
     键：key
     值：value
+ 2、创建 NSDictionary 类
+    (1)、直接创建，创建一个空的 NSDictionary
+    NSDictionary *dict = [NSDictionary dictionary];
+    (2)、创建一对键值对的 NSDictionary
+    NSD
  */
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+        // 1、直接创建一个空 dict （没有实际意义）
+        NSDictionary *dict = [NSDictionary dictionary];
+        // 2、创建一个含一个键值对的 dict 方法： dictionaryWithObject:value值 forKey:key值
+        NSDictionary *dict1 = [NSDictionary dictionaryWithObject:@123 forKey:@"num"];
+        // 3、创建一个含多个键值对的 dict 方法： initWithObjects: value值构成的数组 forKeys: key值构成的数组
+        NSDictionary *dict2 = [[NSDictionary alloc] initWithObjects:@[@123, @456] forKeys:@[@"yyy", @"hhh"]];
+        NSLog(@"%@", dict2);
+        NSLog(@"allKeys = %@, allValues = %@", dict2.allKeys, dict2.allValues);
     }
     return 0;
 }
