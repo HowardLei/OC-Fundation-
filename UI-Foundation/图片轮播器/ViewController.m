2688093b09c3033cc862451c8321882dda9098d2
HowardLei <410905481@qq.com>
2018-09-11 21:36:12 +0800

    图片自动滚动

--- UI-Foundation/图片轮播器/ViewController.m
+++ UI-Foundation/图片轮播器/ViewController.m
@@ -3,6 +3,7 @@
 @interface ViewController () <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+@property (strong, nonatomic) NSTimer *timer;
 @end
 
 @implementation ViewController
@@ -23,6 +24,10 @@
         [self.scrollView addSubview:imageView];
     }
     self.scrollView.contentSize = CGSizeMake(images * width, 0);
+    // 2、设置图片自动滚动
+    // 注意：当直接调用 NSTimer 类方法的时候，这个计时器会持续不断的运行。如果中间用户有拖拽这个 View 的时候，如果拖动的时间偏长，就会出现 2 张图片比方法中设计的时间更短的时间经过。
+    // 解决方案：当发现用户拖动的时候，停止计时器
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(autoScroll) userInfo:nil repeats:YES];
 }
 // MARK: 设置 PageControl 的页码 (通过 scrollView 中的 offset 来判断页码的位置)
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
@@ -30,7 +35,27 @@
     // 注意：这个地方的增加的是 1/2 个 scrollView.frame 。而不是 offset 。
     CGFloat halfOffsetX = scrollView.frame.size.width / 2;
     offsetX += halfOffsetX;
-    self.pageControl.currentPage = offsetX / scrollView.frame.size.width;
+    self.pageControl.currentPage = (NSInteger) (offsetX / scrollView.frame.size.width);
+}
+// MARK: 检测用户是否拖动 scrollView
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    // 当发现用户拖动的时候，就调用 invalidate 方法。停止计时器。
+    [self.timer invalidate];
+    // 当计时器停止了以后，上边的计时器对象就没用了，就需要指向一个新的计时器对象才能重新计时。
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(autoScroll) userInfo:nil repeats:YES];
+}
+// MARK: 实现自动滚动
+- (void)autoScroll {
+    // 每次执行这个方法，让页面滚动一页。
+    // 注意：滚动是靠偏移量来执行的。
+    NSInteger page = self.pageControl.currentPage;
+    if (page == self.pageControl.numberOfPages - 1) {
+        page = 0;
+    } else {
+        page++;
+    }
+    [self.scrollView setContentOffset:CGPointMake(page * self.scrollView.frame.size.width, 0) animated:YES];
+
 }
 
 - (void)didReceiveMemoryWarning {
