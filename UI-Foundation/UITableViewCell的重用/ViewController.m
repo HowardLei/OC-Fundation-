fc52581aa310ed529eb9f94047145ec26eadb670
HowardLei <410905481@qq.com>
2018-09-25 19:17:12 +0800

    有关 cell 的重用

--- UI-Foundation/UITableViewCell的重用/ViewController.m
+++ UI-Foundation/UITableViewCell的重用/ViewController.m
@@ -2,7 +2,12 @@
 #import "ITHero.h"
 /*
  为什么要对 UITableView 中的 Cell 进行重用？
-    因为创建 cell 的时候，
+    因为滚动的时候，都会经历 cell 的创建和回收，不显示的内容删除，显示的对象被创建。这个操作虽然对内存的影响不大，但会不断的进行内存的开辟与销毁，消耗 CPU 的性能。所以需要对 Cell 进行更高效的回收利用。
+ 对 Cell 进行重用的时候，思路如下：
+    1、在创建单元格的时候指定一个“重用 ID ”
+    2、在需要一个单元格的时候，先去缓存池中根据“重用 ID ”查找是否有可用的单元格
+    3、如果有，则直接从缓冲池中取出这个单元格进行使用（修改这个单元格中的样式以及内容）
+       如果没有，此时则需要重新创建一个单元格
  */
 @interface ViewController () <UITableViewDataSource>
 @property (nonatomic, strong) NSArray *arr;
@@ -34,7 +39,13 @@
     // 1、导入数据
     ITHero *model = self.arr[indexPath.row];
     // 2、创建对象
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:nil];
+    NSString *ID = @"HeroID";
+    // 根据上边创建的 ID 去缓存区中寻找对应的 Cell，注意：这个操作是由这个方法内部完成的。
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    // 判断缓存区中有没有 cell ，没有则创建一个
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+    }
     // 2、为对象设置数据
     cell.imageView.image = [UIImage imageNamed:model.icon];
     cell.textLabel.text = model.name;
@@ -47,6 +58,5 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 }
-
 
 @end
