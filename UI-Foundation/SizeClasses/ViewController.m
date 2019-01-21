8d40c733e62941d36bc76a1d7df2c73bb5919df2
雷维卡 <howardlei@howardleis-macbook-pro.local>
2019-01-21 19:46:01 +0800

    Size classes

--- UI-Foundation/SizeClasses/ViewController.m
+++ UI-Foundation/SizeClasses/ViewController.m
@@ -6,7 +6,10 @@
 //
 
 #import "ViewController.h"
-
+/*
+ Size classes
+ 1、出现情况：随着屏幕种类的不断发展，需要适配的屏幕越来越多。
+ */
 @interface ViewController ()
 
 @end
@@ -17,6 +20,5 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
 
 @end
