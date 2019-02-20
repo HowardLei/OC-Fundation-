8588cfc9b46d1344e60680c68b7a080bda366fa3
HowardLei <410905481@qq.com>
2019-02-20 22:57:36 +0800

    UIApplication类

---
+++ UI-Advanced/UIApplication类/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  UIApplication类
+//
+//  Created by 雷维卡 on 2019/2/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UI-Advanced/UIApplication类/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UIApplication类
+//
+//  Created by 雷维卡 on 2019/2/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (IBAction)touchMe {
+    UIApplication *app = UIApplication.sharedApplication;
+}
+@end
