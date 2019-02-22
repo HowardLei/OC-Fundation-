120be2c596df65356e892b1838e60710a8f746e1
HowardLei <410905481@qq.com>
2019-02-23 00:18:04 +0800

    通知原理

--- UI-Advanced/UIApplication类/ViewController.m
+++ UI-Advanced/UIApplication类/ViewController.m
@@ -7,7 +7,7 @@
 
 #import "ViewController.h"
 #import <UserNotifications/UserNotifications.h>
-@interface ViewController ()
+@interface ViewController () <UNUserNotificationCenterDelegate>
 
 @end
 
@@ -19,8 +19,8 @@
 
 - (IBAction)touchMe {
     UIApplication *app = UIApplication.sharedApplication;
-    // 应用程序打开程序，调用 - (void)openURL:NSURL对象 options:<UIApplicationOpenExternalURLOptionsKey, id> 键值对字典 completionHandler:打开完成之后执行的方法。
-    [app openURL:[NSURL URLWithString:@"https://www.baidu.com"] options:@{UIApplicationOpenURLOptionUniversalLinksOnly: @NO} completionHandler:^(BOOL success) {
+    // 应用程序打开程序，调用 - (void)openURL:NSURL对象 options:<UIApplicationOpenExternalURLOptionsKey, id> 键值对字典 completionHandler:打开完成之后执行的代码块（该代码块将会异步执行在主线程当中）。
+    [app openURL:[NSURL URLWithString:@"https://www.baidu.com"] options:@{UIApplicationOpenURLOptionUniversalLinksOnly: @YES} completionHandler:^(BOOL success) {
         if (success) {
             NSLog(@"yes");
         } else {
@@ -28,11 +28,18 @@
         }
     }];
     // 设置图标上的显示的通知数
+    /*
+     当设置图标上的通知数的时候，需要导入用户通知框架（#import <UserNotifications/UserNotifications.h>）因为手机上边的小红标意味着通知数量。而在 iOS 10 以上的版本当中，不管本地通知还是远程通知全部都由 UserNotification 框架来进行管理，原有方法作废。
+     当创建好控制中心以后，需要为控制中心注册认证（即需要向用户索取什么权限，索取完以后在非主线程异步处理事件）。
+     */
     UNUserNotificationCenter *center = UNUserNotificationCenter.currentNotificationCenter;
+    center.delegate = self;
     [center requestAuthorizationWithOptions:UNAuthorizationOptionProvidesAppNotificationSettings completionHandler:^(BOOL granted, NSError * _Nullable error) {
     }];
-    NSSet<UNNotificationCategory *> *category = [NSSet setWithObjects:@"123", nil];
-    [center setNotificationCategories:category];
+    UNNotificationAction *action = [UNNotificationAction actionWithIdentifier:@"id" title:@"今日大事" options:UNNotificationActionOptionNone];
+    UNNotificationCategory *category = [UNNotificationCategory categoryWithIdentifier:@"id" actions:@[action] intentIdentifiers:@[@"nil"] options:UNNotificationCategoryOptionNone];
+    NSSet<UNNotificationCategory *> *categorySet = [NSSet setWithObjects:category, nil];
+    [center setNotificationCategories:categorySet];
     app.applicationIconBadgeNumber = 10;
 }
 @end
