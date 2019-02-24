3f35129424c83c9a622bfca44298c6b94dcd34d8
ITCenter <410905481@qq.com>
2019-02-24 19:32:39 +0800

    提交

---
+++ UI-Advanced/3种方式创建控制器/ITViewController.h
@@ -0,0 +1,16 @@
+//
+//  ITViewController.h
+//  3种方式创建控制器
+//
+//  Created by apple on 2019/2/24.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ITViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UI-Advanced/3种方式创建控制器/ITViewController.m
@@ -0,0 +1,20 @@
+//
+//  ITViewController.m
+//  3种方式创建控制器
+//
+//  Created by apple on 2019/2/24.
+//
+
+#import "ITViewController.h"
+
+@interface ITViewController ()
+
+@end
+
+@implementation ITViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+@end
