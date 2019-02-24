895499bb8fbacee30d9d0b505a1179b224bec939
ITCenter <410905481@qq.com>
2019-02-24 18:49:05 +0800

    新控件

---
+++ UI-Advanced/3种方式创建控制器/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  3种方式创建控制器
+//
+//  Created by apple on 2019/2/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UI-Advanced/3种方式创建控制器/ViewController.m
@@ -0,0 +1,21 @@
+//
+//  ViewController.m
+//  3种方式创建控制器
+//
+//  Created by apple on 2019/2/24.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
