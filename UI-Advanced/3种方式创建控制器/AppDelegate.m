d7b645c0fd552332bd33a26e5783472a0375504d
HowardLei <410905481@qq.com>
2019-02-25 15:24:49 +0800

    控制器代码总结

--- UI-Advanced/3种方式创建控制器/AppDelegate.m
+++ UI-Advanced/3种方式创建控制器/AppDelegate.m
@@ -14,11 +14,28 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [self usingCodeToCreateViewController];
+    [self usingStoryboardCreateViewController];
     return YES;
 }
+/**
+ 用代码创建控制器
+ */
 - (void)usingCodeToCreateViewController {
     self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
     self.window.rootViewController = [[ITViewController alloc] init];
+}
+/**
+ 用 storyboard 创建控制器
+ */
+- (void)usingStoryboardCreateViewController {
+    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
+    UIStoryboard *board = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    // 注意：设置 rootController 有两种设置方法
+    /*
+     第一种：在 storyboard 上设置成初始化启动器。代码设置步骤如下：
+        1. 
+     第二种：在 storyboard 的控制器里面设置 Storyboard ID 即可。
+     */
+    self.window.rootViewController = [board instantiateInitialViewController];
 }
 @end
