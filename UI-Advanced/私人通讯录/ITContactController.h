c06ff854d8f241502a4e66b252a2c2d94900bebd
HowardLei <410905481@qq.com>
2019-03-11 09:08:23 +0800

    代码优化

--- UI-Advanced/私人通讯录/ITContactController.h
+++ UI-Advanced/私人通讯录/ITContactController.h
@@ -10,6 +10,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ITContactController : UITableViewController {
+    @private
     NSMutableArray<ITContact *> *_contacts;
 }
 @property (nonatomic, strong, readonly) NSMutableArray<ITContact *> *contacts;
