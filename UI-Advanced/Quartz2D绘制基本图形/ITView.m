5a64056a9b8eb90822254aabbbe490de966ba973
ITCenter <410905481@qq.com>
2019-03-22 21:30:05 +0800

    画圆形实现代码

--- UI-Advanced/Quartz2D绘制基本图形/ITView.m
+++ UI-Advanced/Quartz2D绘制基本图形/ITView.m
@@ -10,7 +10,7 @@
 @implementation ITView
 
 - (void)drawRect:(CGRect)rect {
-    [self drawCornerRect];
+    [self drawCircle];
 }
 /**
  画长方形
@@ -41,10 +41,19 @@
  */
 - (void)drawCornerRect {
     // 1. 创建 UIBezierPath 路径
-    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(20, 20, 60, 60) cornerRadius:30];
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(20, 20, 60, 60) cornerRadius:10];
     // 2. 绘制图形
     // 注意：当需要修改绘制图形的颜色的时候，调用 UIColor 类的对象的 - (void)set; 方法
     [[UIColor greenColor] set];
+    [path fill];
+}
+/**
+ 画圆形
+ */
+- (void)drawCircle {
+    CGFloat radius = 30;
+    [[UIColor redColor] set];
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(40, 40, radius * 2, radius * 2) cornerRadius:radius];
     [path fill];
 }
 @end
