b1086e1559a8e008de57c6552ce51ea194cb7c27
ITCenter <410905481@qq.com>
2019-03-25 19:38:05 +0800

    代码总结

--- UI-Advanced/Quartz2D渲染模式/ITView.m
+++ UI-Advanced/Quartz2D渲染模式/ITView.m
@@ -8,21 +8,29 @@
 #import "ITView.h"
 
 @implementation ITView
-
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
+    [self drawDemoWithUIBezithPath];
+}
+- (void)drawDemoWithUIBezithPath {
+    [[UIColor redColor] set];
+    UIBezierPath *path1 = [UIBezierPath bezierPathWithRect:CGRectMake(100, 100, 100, 100)];
+    UIBezierPath *path2 = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(100, 100, 100, 100) cornerRadius:100];
+    [path2 appendPath:path1];
+    path2.usesEvenOddFillRule = YES;
+    [path2 fill];
+}
+- (void)drawDemoWithCGContext {
     CGContextRef ref = UIGraphicsGetCurrentContext();
     CGPoint center = CGPointMake(150, 150);
     CGFloat radius = 100;
     UIBezierPath *path1 = [UIBezierPath bezierPathWithArcCenter:center radius:radius startAngle:0 endAngle:M_PI * 2 clockwise:YES];
     UIBezierPath *path2 = [UIBezierPath bezierPathWithArcCenter:center radius:radius / 2 startAngle:0 endAngle:M_PI * 2 clockwise:NO];
-    UIBezierPath *path3 = [UIBezierPath bezierPathWithRect:CGRectMake(100, 100, 10, 100)];
     CGContextAddPath(ref, path1.CGPath);
     CGContextAddPath(ref, path2.CGPath);
-    CGContextAddPath(ref, path3.CGPath);
-    CGContextDrawPath(ref, kCGPathEOFill);
+    // 当设置绘制图形的时候，在第二个参数上设置图形的绘制方式。
+    CGContextDrawPath(ref, kCGPathFill);
 }
-
 @end
