6fc8a0069a894b762a71dcf31ac612b2016fadf7
ITCenter <410905481@qq.com>
2019-03-27 21:16:29 +0800

    饼状图数据绘制

--- UI-Advanced/饼状图/ITView.m
+++ UI-Advanced/饼状图/ITView.m
@@ -13,6 +13,20 @@
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
+    // 将每部分大小保存在数组当中
+    NSArray<NSNumber *> *numbers = @[@30, @15, @5, @17, @3, @10, @20];
+    CGPoint center = CGPointMake(CGRectGetWidth(self.bounds) / 2, CGRectGetHeight(self.bounds) / 2);
+    CGFloat radius = MIN(center.x, center.y);
+    CGFloat start = 0;
+    CGFloat end = 0;
+    UIBezierPath *path = nil;
+    for (NSNumber *number in numbers) {
+        [[UIColor colorWithRed:arc4random_uniform(256) / 255.0 green:arc4random_uniform(256) / 255.0 blue:arc4random_uniform(256) / 255.0 alpha:1] setFill];
+        end = start + (number.doubleValue / 100) * 2 * M_PI;
+        path = [UIBezierPath bezierPathWithArcCenter:center radius:radius startAngle:start endAngle:end clockwise:YES];
+        [path addLineToPoint:center];
+        start = end;
+        [path fill];
+    }
 }
-
 @end
