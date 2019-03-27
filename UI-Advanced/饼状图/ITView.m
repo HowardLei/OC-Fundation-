0f425247139476e0c714e55a6e7d0e4a4d29748e
ITCenter <410905481@qq.com>
2019-03-27 21:50:48 +0800

    代码补充

--- UI-Advanced/饼状图/ITView.m
+++ UI-Advanced/饼状图/ITView.m
@@ -20,10 +20,18 @@
     CGFloat start = 0;
     CGFloat end = 0;
     UIBezierPath *path = nil;
+    /*
+     绘制饼状图算法总结
+     1. 获得饼状图各个部分的数据
+     2. 设置圆心、半径
+     3. 创建起点、终点、路径变量
+     4. 通过 for 循环，不断计算出起点与终点坐标，路径为弧线。设置给创建好的 UIBezierPath 对象
+     */
     for (NSNumber *number in numbers) {
         [[UIColor colorWithRed:arc4random_uniform(256) / 255.0 green:arc4random_uniform(256) / 255.0 blue:arc4random_uniform(256) / 255.0 alpha:1] setFill];
         end = start + (number.doubleValue / 100) * 2 * M_PI;
         path = [UIBezierPath bezierPathWithArcCenter:center radius:radius startAngle:start endAngle:end clockwise:YES];
+        // 注意：绘制的时候，千万不要忘了将弧线的终点与圆心之间相连接。否则绘制的图形是扇形。
         [path addLineToPoint:center];
         start = end;
         [path fill];
