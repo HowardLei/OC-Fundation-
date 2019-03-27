648c226843e2a67042b26bb18af722f3b982de7c
ITCenter <410905481@qq.com>
2019-03-27 22:20:51 +0800

    代码填充

---
+++ UI-Advanced/柱状图/ITView.h
@@ -0,0 +1,16 @@
+//
+//  ITView.h
+//  柱状图
+//
+//  Created by apple on 2019/3/27.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ITView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UI-Advanced/柱状图/ITView.m
@@ -0,0 +1,23 @@
+//
+//  ITView.m
+//  柱状图
+//
+//  Created by apple on 2019/3/27.
+//
+
+#import "ITView.h"
+
+@implementation ITView
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+    NSArray<NSNumber *> *numbers = @[@300, @150.65, @55.3, @507.7, @95.8, @700, @650.65];
+    UIBezierPath *path = nil;
+    for (NSNumber *number in numbers) {
+        
+    }
+}
+
+@end
