bffc9f4521e0795ae768b756650e684eb6db4a4c
HowardLei <410905481@qq.com>
2019-04-01 20:34:37 +0800

    柱状图绘制成功

--- UI-Advanced/柱状图/ITView.m
+++ UI-Advanced/柱状图/ITView.m
@@ -13,11 +13,17 @@
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
+    [[UIColor redColor] set];
     NSArray<NSNumber *> *numbers = @[@300, @150.65, @55.3, @507.7, @95.8, @700, @650.65];
+    NSUInteger count = numbers.count;
     UIBezierPath *path = nil;
-    for (NSNumber *number in numbers) {
-        
+    CGFloat margin = 10;
+    CGFloat width = (CGRectGetWidth(rect) - (count - 1) * margin) / count;
+    CGFloat height = 0;
+    for (NSUInteger i = 0; i < count; i++) {
+        height = numbers[i].floatValue * 0.3;
+        path = [UIBezierPath bezierPathWithRect:CGRectMake(width * i + margin * (i - 1), 300 - height, width, height)];
+        [path fill];
     }
 }
-
 @end
