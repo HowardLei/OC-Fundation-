b0950df57b0e267f3f763c228087862565c68fcf
ITCenter <410905481@qq.com>
2019-04-16 13:12:27 +0800

    多线程的执行状态

--- 多线程/NSThread/ViewController.m
+++ 多线程/NSThread/ViewController.m
@@ -31,10 +31,23 @@
     // 3. 隐式创建线程
     [self performSelectorInBackground:@selector(run) withObject:nil];
 }
+/**
+ 线程的生命周期
+ */
 - (void)demo2 {
-    
+    // 1. 新建线程
+    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(run) object:nil];
+    // 2. 将线程添加到可执行线程池当中，线程变成 runnable 状态
+    [thread start];
+    // 如果中途没有特殊情况，CPU 会在一个时间点执行该线程。
 }
 - (void)run {
     NSLog(@"%@", NSThread.currentThread);
+
+    // 3. 中途休眠该线程，使线程进入 block 状态
+    [NSThread sleepForTimeInterval:3];
+    // 当休眠完成以后，线程将会进入 runnable 状态，等待 CPU 再次调用，进入 running 状态
+    // 4. 当需要直接手动杀死当前线程，调用 + (void)exit; 方法
+    [NSThread exit];
 }
 @end
