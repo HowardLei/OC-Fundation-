8c7ee06b3af44b74476cfb6c2983d626c88248ee
ITCenter <410905481@qq.com>
2019-04-21 18:47:33 +0800

    代码优化

--- UI-Advanced/文字与图片的绘制/ITView.m
+++ UI-Advanced/文字与图片的绘制/ITView.m
@@ -8,7 +8,6 @@
 #import "ITView.h"
 
 @implementation ITView
-
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
