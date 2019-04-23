551730bc3e1d99fa5c802dcd154c8f12cc7f7093
ITCenter <410905481@qq.com>
2019-04-23 18:57:16 +0800

    UITabBarController代码实现

---
+++ UI-Advanced/UITabBarController基本原理/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  UITabBarController基本原理
+//
+//  Created by apple on 2019/4/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UI-Advanced/UITabBarController基本原理/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  UITabBarController基本原理
+//
+//  Created by apple on 2019/4/23.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
