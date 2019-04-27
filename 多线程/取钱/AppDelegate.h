7ad28cf776b1b1317b6b049c988074146ad9aaf2
ITCenter <410905481@qq.com>
2019-04-27 21:15:57 +0800

    取钱的创建

---
+++ 多线程/取钱/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  取钱
+//
+//  Created by apple on 2019/4/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 多线程/取钱/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  取钱
+//
+//  Created by apple on 2019/4/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
